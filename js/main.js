f07573b61a8738e06ba439a724ceb766ca86030a
gustavo <gsarruda.office@gmail.com>
2023-07-12 21:40:40 -0300

    lite-server

--- js/main.js
+++ js/main.js
@@ -7,11 +7,6 @@ router.add("/universe", "/pages/universe.html");
 router.add("/exploration", "/pages/exploration.html");
 router.add(404, "/pages/404.html");
 
-router.handle();
-
-window.onpopstate = () => router.handle();
-window.route = () => router.route();
-
 const controls = Controls({
   home,
   universe,
@@ -44,3 +39,8 @@ exploration.addEventListener("click", () => {
     htmlElement.style.backgroundSize = "cover";
   }
 });
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
